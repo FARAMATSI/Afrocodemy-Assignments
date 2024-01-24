c9a890972f6ce326cc9a3a33289881c3e5cafd8e
Fasoft <101709684+FARAMATSI@users.noreply.github.com>
2024-01-24 15:44:59 +0200

    Update Main.java

--- Afrocodemy Assignment2 17 January 2024 (Arrays)/Question2/Main.java
+++ Afrocodemy Assignment2 17 January 2024 (Arrays)/Question2/Main.java
@@ -2,16 +2,16 @@ public class Main{
 	public static void main(String[] args){
 		int[] integers = {1,2,3,3,2,1};
 		int maxIndex = integers.length-1;
-		boolean pairsEqual = false;
+		boolean arrayReadsTheSameBackAndFront = false;
 		for(int i =0; i<integers.length; i++){
 			if(integers[i] == integers[maxIndex-i]){
-			pairsEqual = true;
+			arrayReadsTheSameBackAndFront = true;
 		}
 			else{
-				pairsEqual = false;
+				arrayReadsTheSameBackAndFront = false;
 				}
 		}
-		if(pairsEqual == true){
+		if(arrayReadsTheSameBackAndFront == true){
 					System.out.println("The array reads the same in all directions");
 					}
 					else{
