bab9fde2aed09e7f11ea1859997dda6f8d3e95d5
Fasoft <101709684+FARAMATSI@users.noreply.github.com>
2024-01-24 16:07:17 +0200

    Update Main.java (Variable naming)

--- Afrocodemy Assignment2 17 January 2024 (Arrays)/Question3/Main.java
+++ Afrocodemy Assignment2 17 January 2024 (Arrays)/Question3/Main.java
@@ -2,10 +2,10 @@ public class Main{
     public static void main(String[] args){
         int[] firstArray = {1,2,3,4,5,6,7,8,9,10};
         int maxIndex = firstArray.length-1;
-        int[] secondArray = new int[10];
+        int[] reversedArray = new int[10];
 
         for(int i = maxIndex,secondArrayIterator=0; i>=0; i--){
-            secondArray[secondArrayIterator++] = firstArray[i];
+            reversedArray[reversedArrayIterator++] = firstArray[i];
         }
 
 
@@ -13,7 +13,7 @@ public class Main{
             System.out.print(" "+i);
         }
         System.out.println("\n");
-        for(int i : secondArray){
+        for(int i : reversedArray){
             System.out.print(" "+i);
         }
         System.out.println("\n");
